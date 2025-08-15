e8cf95a20a47887a09e7ea819c8ae118e9baeb03
lohls <loresantos55@gmail.com>
2025-08-15 17:32:14 -0300

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,9 +23,44 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Camila");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + dev1.getConteudosIncritos());
+        dev1.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos" + dev1.getConteudosIncritos());
+        System.out.println("Conteúdos Concluidos" + dev1.getConteudosConcluidos());
+        System.out.println("XP:" + dev1.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev dev2 = new Dev();
+        dev1.setNome("João");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + dev2.getConteudosIncritos());
+        dev2.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos" + dev2.getConteudosIncritos());
+        System.out.println("Conteúdos Concluidos" + dev2.getConteudosConcluidos());
+        System.out.println("XP:" + dev2.calcularTotalXp());
+
+
+
+
+
 
     }
 }
